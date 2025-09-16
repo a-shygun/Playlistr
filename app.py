d10b3052a45e56b4596471bb57ba8368f599f23e
aryanshygun <ryan.shygun@gmail.com>
2025-09-16 22:03:24 +0330

    removed __name__

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(views_bp)
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5000)
